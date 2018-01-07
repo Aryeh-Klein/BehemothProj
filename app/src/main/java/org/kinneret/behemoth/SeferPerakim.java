90930ab3ff6cf49bf79acca8a1d23e44fb66de2e
MosheWGit <mweinreb212@gmail.com>
2018-01-07 00:42:48 -0500

    01/07/18_00:41 dynamic hebrew buttons

--- app/src/main/java/org/kinneret/behemoth/SeferPerakim.java
+++ app/src/main/java/org/kinneret/behemoth/SeferPerakim.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -51,10 +52,15 @@ public class SeferPerakim extends AppCompatActivity {
 
     private void createLayoutDynamically(Integer n) {
 
-        for (int i = 0; i < n; i++) {
+
+        //Toast.makeText(SeferPerakim.this, converted, Toast.LENGTH_LONG).show();
+
+        for (int i = 1; i <= n; i++) {
             Button myButton = new Button(this);
-            myButton.setText("Button :" + i);
+            String converted = convertIntToHeb(i);
+            myButton.setText("פרק " + converted);
             myButton.setId(i);
+            myButton.setWidth(LinearLayout.LayoutParams.WRAP_CONTENT);
             final int id_ = myButton.getId();
 
             final LinearLayout layout = (LinearLayout) findViewById(R.id.PerakimList);
@@ -69,6 +75,54 @@ public class SeferPerakim extends AppCompatActivity {
         }
 
 
+    }
+
+    private String convertIntToHeb(Integer n){
+        char char1 = (char) 1514;
+        char char2 = (char) 1514;
+        Integer chardig2 = n % 10;;
+        String converted = null;
+
+        if(n < 10){
+            char1 = (char) (1487 + n);
+        }
+        else if(n >= 10 && n < 20){
+            char1 = (char) 1497;
+            char2 = (char) (1487 + chardig2);
+
+        }
+        else if(n >= 20 && n < 30){
+            char1 = (char) 1499;
+            char2 = (char) (1487 + chardig2);
+
+        }
+        else if(n >= 30 && n < 40){
+            char1 = (char) 1500;
+            char2 = (char) (1487 + chardig2);
+
+        }
+        else if(n >= 40 && n < 50){
+            char1 = (char) 1502;
+            char2 = (char) (1487 + chardig2);
+
+        }
+        else if(n >= 50){
+            char1 = (char) 1504;
+            //char2 = (char) (1487 + chardig2);
+
+        }
+
+
+        if(chardig2 == 0 || n < 10){
+            converted = Character.toString(char1);
+        }
+        else{
+            converted =  Character.toString(char1) + Character.toString(char2);
+
+        }
+        //converted =  char2;
+       // Log.d("Printable",converted);
+        return converted;
     }
 
 }
