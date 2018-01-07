1f99f8eeb4c986ea81fc4f31bfd8db9cf17be67a
Aryeh-Klein <aryehklein01@gmail.com>
2018-01-07 09:30:11 -0500

    Temp

---
+++ app/src/main/java/org/kinneret/behemoth/Perakim.java
@@ -0,0 +1,22 @@
+package org.kinneret.behemoth;
+
+/**
+ * Created by Aryeh on 1/5/2018.
+ */
+
+public final class Perakim {
+
+    public Perakim(){
+
+    }
+
+    public static String [] getPerakimList(){
+        return new String [] {
+                "perek 1",
+                "perek 2,",
+                "perek 3",
+                "perek 4"
+
+        };
+    }
+}
--- app/src/main/java/org/kinneret/behemoth/SeferPage.java
+++ app/src/main/java/org/kinneret/behemoth/SeferPage.java
@@ -5,6 +5,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SeferPage extends AppCompatActivity {
@@ -35,6 +36,9 @@ public class SeferPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sefer_page);
+
+        final Button myButton;
+        myButton = (Button)(R.id.per);
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
