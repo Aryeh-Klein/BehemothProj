996defcf9e0550ec7ad01252bcb9bd3268fdd955
MosheWGit <mweinreb212@gmail.com>
2018-02-11 23:54:53 -0500

    02/11/18_23:54 Shmot 32 first testing

--- app/src/main/java/org/kinneret/behemoth/PesukimPage.java
+++ app/src/main/java/org/kinneret/behemoth/PesukimPage.java
@@ -11,6 +11,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 public class PesukimPage extends AppCompatActivity {
@@ -29,7 +33,30 @@ public class PesukimPage extends AppCompatActivity {
 
         pesukim = (TextView) findViewById(R.id.pesukim);
 
-        setUpPage(sefer, perek, number);
+        if(sefer.equals("Shmot") && perek == 32){
+            /*try{
+                StringBuilder buf = new StringBuilder();
+                InputStream json = getAssets().open("Shmot32.txt");
+                BufferedReader in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
+                String str;
+
+                while ((str = in.readLine()) != null) {
+                    buf.append(str);
+                }
+
+                in.close();
+                pesukim.setText(buf.toString());
+            }
+            catch(IOException e){
+
+            }*/
+            pesukim.setText(R.string.shmot32);
+
+        }
+        else{
+            setUpPage(sefer, perek, number);
+        }
+
 
 
     }
@@ -42,12 +69,21 @@ public class PesukimPage extends AppCompatActivity {
                 String perekFinder = "Chapter " + perekid;
                 String tempUri = "https://www.mechon-mamre.org/p/pt/pt0" + num + ".htm";
                 URI machonSefer =  URI.create(tempUri);
+                InputStream is = null;
 
                 try{
                     Log.d("Pesukim","reached");
-                    Document doc  = Jsoup.connect(tempUri).get();
+                    /*Document doc  = Jsoup.connect(tempUri).get();*/
+                    is = getAssets().open("bereishitheb.html");
+                    Document doc = Jsoup.parse(is,"UTF-8","https://www.example.com");
                     Elements perakim = doc.getElementsByTag("H2");
-                    for(Element perek: perakim){
+                    Element perek = perakim.get(perekid - 1);
+                    Element table = perek.nextElementSibling();
+                    Elements pesukimHebrew = table.getElementsByClass("h");
+                    for(Element pasuk : pesukimHebrew){
+                        builder.append(pasuk.text()).append("\n");
+                    }
+                    /*for(Element perek: perakim){
                         String current = perek.text();
                         if(current.equals(perekFinder)){
                             Element table = perek.nextElementSibling();
@@ -56,7 +92,7 @@ public class PesukimPage extends AppCompatActivity {
                                 builder.append(pasuk.text()).append("\n");
                             }
                         }
-                    }
+                    }*/
 
                 }catch(Exception e){
                     e.printStackTrace();
